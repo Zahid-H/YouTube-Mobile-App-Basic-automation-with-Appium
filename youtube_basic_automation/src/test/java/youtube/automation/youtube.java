a0c263a14be9c5bcb480182cc0f4facfd06a9cc7
Zahid Hasan Juel <83463788+Zahid-H@users.noreply.github.com>
2023-08-01 17:24:47 +0600

    capabilities are being set in the setup class

--- youtube_basic_automation/src/test/java/youtube/automation/youtube.java
+++ youtube_basic_automation/src/test/java/youtube/automation/youtube.java
@@ -1,11 +1,29 @@
 package youtube.automation;
 
+import java.net.URL;
+
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeTest;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
 public class youtube {
+    public static AppiumDriver<MobileElement> driver;
+    String url;
+
     @BeforeTest
     public void setup() {
 
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("deviceName", "emulator-5554");
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("appPackage", "com.daraz.android");
+        capabilities.setCapability("appActivity", "com.lazada.android.maintab.DarazEntryActivity");
+        capabilities.setCapability("noReset", true);
+        url = "http://127.0.0.1:4723/wd/hub";
+        driver = new AndroidDriver<MobileElement>(new URL(url), capabilities);
     }
 
 }
